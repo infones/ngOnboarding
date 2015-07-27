67e35a25cf31bff9c75ecbdd0bfbad54251ac3a0
Epick362 <flp.hajek@gmail.com>
2015-07-27 15:43:34 +0200

    Positioning Revert

--- dist/ng-onboarding.js
+++ dist/ng-onboarding.js
@@ -146,7 +146,7 @@
                 } else if (scope.position === 'left') {
                   right = $(window).width() - $(attachTo).offset().left + xMargin;
                 } else if (scope.position === 'top' || scope.position === 'bottom') {
-                  left = $(attachTo).offset().left + $(attachTo).outerWidth() / 2 - $('.onboarding-popover').outerWidth() / 2;
+                  left = $(attachTo).offset().left;
                 }
                 if (curStep['xOffset']) {
                   if (left !== null) {
